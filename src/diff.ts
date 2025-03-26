3766c87efaef3ee15c94a68df693be96026db91a
again13 <againhoffman@yahoo.com>
2025-03-26 22:35:49 +0000

    Update configuration - removing deprecated function

--- src/diff.ts
+++ src/diff.ts
@@ -16,29 +16,29 @@ function diff(thing1, thing2) {
 }
 
 export async function diffCmd(first: string, second: string): Promise<void> {
-	let firstMeta = await parseTorrentFromFilename(first);
-	let secondMeta = await parseTorrentFromFilename(second);
-	let firstRes = createSearcheeFromMetafile(firstMeta);
+	const firstMeta = await parseTorrentFromFilename(first);
+	const secondMeta = await parseTorrentFromFilename(second);
+	const firstRes = createSearcheeFromMetafile(firstMeta);
 	if (firstRes.isErr()) {
 		console.log(firstRes.unwrapErr());
 		return;
 	}
-	let secondRes = createSearcheeFromMetafile(secondMeta);
+	const secondRes = createSearcheeFromMetafile(secondMeta);
 	if (secondRes.isErr()) {
 		console.log(secondRes.unwrapErr());
 		return;
 	}
-	let s1 = firstRes.unwrap();
-	let s2 = secondRes.unwrap();
-	let sortBy =
+	const s1 = firstRes.unwrap();
+	const s2 = secondRes.unwrap();
+	const sortBy =
 		s1.files.length === 1
 			? (a, b) => b.length - a.length
 			: s2.files.length === 1
 				? (a, b) => a.length - b.length
 				: (a, b) => a.path.localeCompare(b.path);
 
-	let stripForDiff = (searchee: Searchee) => {
-		for (let key of Object.keys(searchee)) {
+	const stripForDiff = (searchee: Searchee) => {
+		for (const key of Object.keys(searchee)) {
 			if (key !== "files") delete searchee[key];
 		}
 		searchee.files.sort(sortBy);
